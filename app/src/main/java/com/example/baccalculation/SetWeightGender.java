31ef5e5dd4dcf1e0b016c94aa5142a1154ed4b0f
Linh <thung2@uncc.edu>
2022-09-06 10:07:19 -0400

    Initial commit

---
+++ app/src/main/java/com/example/baccalculation/SetWeightGender.java
@@ -0,0 +1,14 @@
+package com.example.baccalculation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetWeightGender extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_weight_gender);
+    }
+}
